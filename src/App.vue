f540e4b2f5e527758201308d88d81fb13dc18ca2
MarcosIraola <iraolamarcos@gmail.com>
2022-10-16 17:19:51 -0300

    payment with redirect implemented

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="navbar">
+    <RouterLink class="link" to="/">Home</RouterLink>
+    <RouterLink class="link" to="/about">About</RouterLink>
+  </div>
+
+  <div class="main">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  width: 100%;
+  padding: 20px;
+  background-color: rgb(61, 61, 61);
+}
+
+.link {
+  margin: 10px;
+  text-decoration: none;
+  color: white;
+}
+
+.main {
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '../stores/counter'
+import Checkout from '../components/Checkout.vue'
+
+</script>
+
+<template>
+  <h1>Hello world</h1>
+  <Checkout />
+</template>
+
+<style scoped>
+
+</style>
